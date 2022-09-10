99862e5298aa8091b7b0f7f97bddf4720763a131
izohek <izohek@gmail.com>
2022-09-10 14:17:44 -0700

    Added Bessieres divisions.

--- dist/DivisionData.js
+++ dist/DivisionData.js
@@ -4,10 +4,34 @@ exports.AllDivisions = void 0;
 /// Warno Division data
 const AllDivisions = [
     {
-        name: '35th',
+        name: '7th',
         country: 'SOV',
-        id: 23,
+        id: 9,
         alliance: 'axis'
+    },
+    {
+        name: '5th',
+        country: 'RFA',
+        id: 10,
+        alliance: 'allies'
+    },
+    {
+        name: '79th',
+        country: 'SOV',
+        id: 11,
+        alliance: 'axis'
+    },
+    {
+        name: '3rd',
+        country: 'USA',
+        id: 13,
+        alliance: 'allies'
+    },
+    {
+        name: '8th',
+        country: 'USA',
+        id: 14,
+        alliance: 'allies'
     },
     {
         name: '39th',
@@ -22,40 +46,18 @@ const AllDivisions = [
         alliance: 'axis'
     },
     {
-        name: '79th',
-        country: 'SOV',
-        id: 11,
-        alliance: 'axis'
-    },
-    {
-        name: '7th',
-        country: 'SOV',
-        id: 9,
-        alliance: 'axis'
-    },
-    {
-        name: 'KDA',
-        country: 'DDR',
-        id: 27,
-        alliance: 'axis'
-    },
-    {
         name: '2nd',
         country: 'RFA',
         id: 20,
         alliance: 'allies'
     },
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_39GMRD_solo, 21),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_57GMRD_solo, 22),
     {
-        name: '3rd',
-        country: 'USA',
-        id: 13,
-        alliance: 'allies'
-    },
-    {
-        name: '5th',
-        country: 'RFA',
-        id: 10,
-        alliance: 'allies'
+        name: '35th',
+        country: 'SOV',
+        id: 23,
+        alliance: 'axis'
     },
     {
         name: '82nd',
@@ -64,16 +66,43 @@ const AllDivisions = [
         alliance: 'allies'
     },
     {
-        name: '8th',
-        country: 'USA',
-        id: 14,
+        name: '5e',
+        country: 'FR',
+        id: 26,
         alliance: 'allies'
+    },
+    {
+        name: 'KDA',
+        country: 'DDR',
+        id: 27,
+        alliance: 'axis'
     },
     {
         name: 'TKS',
         country: 'RFA',
         id: 28,
         alliance: 'allies'
+    },
+    {
+        name: '1st',
+        country: 'UK',
+        id: 29,
+        alliance: 'allies'
     }
+    // TODO: SOLO - (Descriptor_Deck_Division_RDA_11MSD_solo, 62),
+    // TODO: SOLO - (Descriptor_Deck_Division_RDA_4MSD_solo, 63),
+    // TODO: SOLO - (Descriptor_Deck_Division_RDA_7PzD_solo, 64),
+    // TODO: SOLO - (Descriptor_Deck_Division_RDA_MBDIII_solo, 65),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_11GTkD_solo, 66),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_1GTA_solo, 67),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_20GMRD_solo, 68),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_27GMRD_solo, 69),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_79GTkD_solo, 70),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_8GA_solo, 71),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_9TkD_solo, 72),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_WGF_solo, 73),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_308ArtBr_solo, 74),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_35GvDSHB_solo, 75),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_390ArtBr_solo, 76),
 ];
 exports.AllDivisions = AllDivisions;
--- src/DivisionData.ts
+++ src/DivisionData.ts
@@ -3,10 +3,34 @@ import Division from './Division'
 /// Warno Division data
 const AllDivisions: Division[] = [
     {
-        name: '35th',
+        name: '7th',
         country: 'SOV',
-        id: 23,
+        id: 9,
         alliance: 'axis'
+    },
+    {
+        name: '5th',
+        country: 'RFA',
+        id: 10,
+        alliance: 'allies'
+    },
+    {
+        name: '79th',
+        country: 'SOV',
+        id: 11,
+        alliance: 'axis'
+    },
+    {
+        name: '3rd',
+        country: 'USA',
+        id: 13,
+        alliance: 'allies'
+    },
+    {
+        name: '8th',
+        country: 'USA',
+        id: 14,
+        alliance: 'allies'
     },
     {
         name: '39th',
@@ -21,41 +45,18 @@ const AllDivisions: Division[] = [
         alliance: 'axis'
     },
     {
-        name: '79th',
-        country: 'SOV',
-        id: 11,
-        alliance: 'axis'
-    },
-    {
-        name: '7th',
-        country: 'SOV',
-        id: 9,
-        alliance: 'axis'
-    },
-    {
-        name: 'KDA',
-        country: 'DDR',
-        id: 27,
-        alliance: 'axis'
-    },
-
-    {
         name: '2nd',
         country: 'RFA',
         id: 20,
         alliance: 'allies'
     },
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_39GMRD_solo, 21),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_57GMRD_solo, 22),
     {
-        name: '3rd',
-        country: 'USA',
-        id: 13,
-        alliance: 'allies'
-    },
-    {
-        name: '5th',
-        country: 'RFA',
-        id: 10,
-        alliance: 'allies'
+        name: '35th',
+        country: 'SOV',
+        id: 23,
+        alliance: 'axis'
     },
     {
         name: '82nd',
@@ -64,17 +65,45 @@ const AllDivisions: Division[] = [
         alliance: 'allies'
     },
     {
-        name: '8th',
-        country: 'USA',
-        id: 14,
+        name: '5e',
+        country: 'FR',
+        id: 26,
         alliance: 'allies'
+    },
+    {
+        name: 'KDA',
+        country: 'DDR',
+        id: 27,
+        alliance: 'axis'
     },
     {
         name: 'TKS',
         country: 'RFA',
         id: 28,
         alliance: 'allies'
+    },
+    {
+        name: '1st',
+        country: 'UK',
+        id: 29,
+        alliance: 'allies'
     }
+    
+    // TODO: SOLO - (Descriptor_Deck_Division_RDA_11MSD_solo, 62),
+    // TODO: SOLO - (Descriptor_Deck_Division_RDA_4MSD_solo, 63),
+    // TODO: SOLO - (Descriptor_Deck_Division_RDA_7PzD_solo, 64),
+    // TODO: SOLO - (Descriptor_Deck_Division_RDA_MBDIII_solo, 65),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_11GTkD_solo, 66),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_1GTA_solo, 67),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_20GMRD_solo, 68),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_27GMRD_solo, 69),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_79GTkD_solo, 70),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_8GA_solo, 71),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_9TkD_solo, 72),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_WGF_solo, 73),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_308ArtBr_solo, 74),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_35GvDSHB_solo, 75),
+    // TODO: SOLO - (Descriptor_Deck_Division_SOV_390ArtBr_solo, 76),
 ]
 
 export { AllDivisions }
